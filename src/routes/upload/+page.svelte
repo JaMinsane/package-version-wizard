c981b6743e20db62c53145a2227f9e8317974f30
JaMinsane <james.jh03@gmail.com>
2026-03-31 12:16:47 -0500

    mejora copy

--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -15,7 +15,7 @@
 </svelte:head>
 
 <div class="min-h-screen px-4 py-6 sm:px-6 lg:px-10">
-	<div class="mx-auto flex max-w-4xl flex-col gap-6 sm:gap-8">
+	<div class="mx-auto flex max-w-3xl flex-col gap-6 sm:gap-8">
 		<UploadStudio environmentReady={data.environmentReady} formMessage={form?.message} />
 	</div>
 </div>
